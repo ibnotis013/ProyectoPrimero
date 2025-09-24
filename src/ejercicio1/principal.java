1e4003815a28511250061e6f9c698ac6203ef20a
ibnotis013 <n.ledo@liceolapaz.net>
2025-09-24 10:15:05 +0200

    Estado inicial de mi proyecto

--- src/ejercicio1/principal.java
+++ src/ejercicio1/principal.java
@@ -1,9 +1,18 @@
 package ejercicio1;
 
+import java.util.Scanner;
+
 public class principal {
     public static void main() {
 
+        Scanner teclado = new Scanner(System.in);
         String nombre= "nicolas";
         System.out.println("mi nombre es " +nombre );
+        nombre= "iker";
+        System.out.println("y el de mi izquierda es " +nombre );
+        System.out.println("introdizca el nombre del cabezon de mi derecha");
+        String nombrepersona=teclado.nextLine();
+        System.out.println("el nombre es "+nombrepersona);
+
     }
 }
