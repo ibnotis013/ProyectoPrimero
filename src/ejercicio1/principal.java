174987881086625d5f222a14673ab1635ec73455
ibnotis013 <n.ledo@liceolapaz.net>
2025-09-24 10:14:14 +0200

    Estado inicial de mi proyecto

---
+++ src/ejercicio1/principal.java
@@ -0,0 +1,9 @@
+package ejercicio1;
+
+public class principal {
+    public static void main() {
+
+        String nombre= "nicolas";
+        System.out.println("mi nombre es " +nombre );
+    }
+}
