ec7cbd88d7c598fa026b2835ec3c0600f81ac4f7
ibnotis013 <n.ledo@liceolapaz.net>
2025-09-26 10:59:25 +0200

    ejercicio 3 acabado

---
+++ src/ejercicio3/principal.java
@@ -0,0 +1,18 @@
+package ejercicio3;
+
+import java.util.Scanner;
+
+public class principal {
+    static void main() {
+        System.out.println("introduzca la edad ");
+        Scanner sc = new Scanner(System.in);
+        int edad = sc.nextInt();
+        if (edad>=18)
+        {
+            System.out.println("puedes pasar ya que eres mayer de edad");
+        }else{
+                System.out.println("eres menor no puedes pasar ");
+            }
+        
+    }
+}
