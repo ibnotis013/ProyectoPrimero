3c3d40c8ed0aa8695351ab2f4ee1ec7bf6c978d3
ibnotis013 <n.ledo@liceolapaz.net>
2025-09-30 13:35:10 +0200

    ejercicio 7 terminado

--- src/ejercicio7/principal.java
+++ src/ejercicio7/principal.java
@@ -6,22 +6,27 @@ public class principal {
     static void main() {
     Scanner sc = new Scanner(System.in);
         System.out.println("introduce un numero del 0 al 10");
-        int numero = sc.nextInt();
+        double numero = sc.nextDouble();
 
-        if (numero <=4) {
-            System.out.println("suspenso");
+        if(!(numero>=0 && numero<=10)){
+            System.out.println("la nota tiene que estar entre 0 y 10");
         }
-        else if (numero==5) {
-            System.out.println("aprobado");
-        }
-        else if (numero==6) {
-            System.out.println("Bien");
-        }
-        else if (numero==7&&numero==8) {
-            System.out.println("Notable");
-        }
-        else if (numero==9&&numero==10){
-            System.out.println("sobresaliente");
+        else{
+            if (numero <5) {
+                System.out.println("suspenso");
+            }
+            else if (numero<6) {
+                System.out.println("aprobado");
+            }
+            else if (numero<7) {
+                System.out.println("Bien");
+            }
+            else if (numero<9) {
+                System.out.println("Notable");
+            }
+            else {
+                System.out.println("sobresaliente");
+            }
         }
     }
 }
